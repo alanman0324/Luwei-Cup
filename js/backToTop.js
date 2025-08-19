4f254b2275ce386d037828d44e69f46bfd39426e
蕭偉綸 <alaman0324@gmail.com>
2025-08-19 21:44:44 +0800

    更新回到頂端圖片及動畫

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,7 +1,11 @@
 const backToTop = document.getElementById('backToTop');
 
 window.addEventListener('scroll', () => {
-    backToTop.style.display = window.scrollY > 300 ? 'block' : 'none';
+    if (window.scrollY > 300) {
+        backToTop.classList.add('show'); // 顯示按鈕
+    } else {
+        backToTop.classList.remove('show'); // 隱藏按鈕
+    }
 });
 
 backToTop.addEventListener('click', (e) => {
